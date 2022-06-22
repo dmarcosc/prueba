ade60f5338589963c61ed9131ca85ac0e0761bba
dmarcosc <carcharoth92@gmail.com>
2022-06-22 18:43:37 +0200

    pruebas

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,11 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: "./",
+  base: "/prueba/",
   resolve: {
     alias: {
       "@": resolve(__dirname, "src")
